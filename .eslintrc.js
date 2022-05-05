79aeca509f21c890357243c36d390c6d554f7cc8
Igor Naydenov <igornaydenov246@gmail.com>
2022-05-06 00:10:50 +0300

    changed eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+const { getBaseConfig } = require("@edx/frontend-build");
+const withDojoPreset = require("@reustleco/dojo-frontend-common/configs/eslint");
+
+module.exports = withDojoPreset(getBaseConfig("eslint"));
+
